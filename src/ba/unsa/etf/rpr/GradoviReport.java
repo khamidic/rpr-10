c4fe47c69ebbfb9cb29fe593e47db8e43abbf289
Kenan Hamidic <kenanhamidic@gmail.com>
2018-12-27 01:45:15 +0100

    gradovi report

---
+++ src/ba/unsa/etf/rpr/GradoviReport.java
@@ -0,0 +1,178 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import net.sf.jasperreports.engine.*;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class GradoviReport implements Initializable {
+    public GeografijaDAO model;
+    public TextField nazivGrada;
+    public Menu View, Language;
+    public TextField brojStanovnikaGrada;
+    public TextField idDrzave;
+    public TextField nazivDrzave;
+    public TextField izGlavnog;
+    public MenuItem Bosanski, Engleski, Francuski, Njemacki;
+    public ResourceBundle b;
+    public Locale l;
+    public Label grad, brStanovnika;
+    public Button stam, dodajDrz, dodajGr;
+
+    public void initialize(URL url, ResourceBundle bundle){
+        b = bundle;
+    }
+
+    public GradoviReport(GeografijaDAO m) { model = m; }
+
+    public void dodajGradDB(javafx.event.ActionEvent actionEvent) {
+        Grad g = new Grad();
+        g.setNaziv(nazivGrada.getText());
+        g.setBrojStanovnika(Integer.parseInt(brojStanovnikaGrada.getText()));
+        Drzava d = new Drzava();
+        d.setId(Integer.parseInt(idDrzave.getText()));
+        g.setDrzava(d);
+
+        model.dodajGrad(g);
+    }
+
+    public void dodajDrzavuDB(javafx.event.ActionEvent actionEvent) {
+        Drzava d = new Drzava();
+        d.setNaziv(nazivDrzave.getText());
+        Grad g = new Grad();
+        g.setId(Integer.parseInt(izGlavnog.getText()));
+        d.setGlavniGrad(g);
+
+        model.dodajDrzavu(d);
+    }
+
+    public void PDF(ActionEvent actionEvent){
+        saveAs("pdf");
+    }
+
+    public void DOCX(ActionEvent actionEvent){
+        saveAs("docx");
+    }
+
+    public void XMLX(ActionEvent actionEvent){
+        saveAs("xmlx");
+    }
+
+    public void saveAs(String format){
+        JasperReport jasperReport = null;
+        JasperPrint jasperPrint = null;
+        if(format.contains("pdf")){
+            try {
+                jasperReport = JasperCompileManager.compileReport(getClass().getResourceAsStream("/reports/gradovi.jrxml"));
+                JRDataSource jrDataSource = new JREmptyDataSource();
+                jasperPrint = JasperFillManager.fillReport(jasperReport, null, jrDataSource);
+                File file = new File("resources/reports/gradovi.jrxml");
+                String s = file.getAbsolutePath();
+                String s1 = s.substring(0, s.length()-5) + "pdf";
+                JasperExportManager.exportReportToPdfFile(jasperPrint, s1);
+            } catch (JRException e) {
+                e.printStackTrace();
+            }
+        }
+        if(format.contains("docx")){
+           /* Exporter exporter = new JRDocxExporter();
+            exporter.setExporterInput(new SimpleExporterInput(String.valueOf(jasperPrint)));
+            File file = new File("resources/reports/gradovi.jrxml");
+            String s = file.getAbsolutePath();
+            String s1 = s.substring(0, s.length()-5) + "docx";
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(s1));
+            try {
+                exporter.exportReport();
+            } catch (JRException e) {
+                e.printStackTrace();
+            }*/
+        } else{
+
+        }
+    }
+
+    public void showReport(javafx.event.ActionEvent actionEvent) {
+        try {
+            new PrintReport().showReport(model.getConn());
+        } catch (JRException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public void bosanskiJezik(ActionEvent actionEvent) {
+        l = new Locale("bs");
+        b = ResourceBundle.getBundle("Translation", l);
+        View.setText(b.getString("ViewPogled"));
+        Bosanski.setText(b.getString("Bos"));
+        Engleski.setText(b.getString("Eng"));
+        Njemacki.setText(b.getString("Njem"));
+        Francuski.setText(b.getString("Fr"));
+        Language.setText(b.getString("LanugagePrevod"));
+        String s = b.getString("NazivGrada");
+        String s1 = b.getString("brSt");
+        grad.setText(s);
+        brStanovnika.setText(s1);
+        stam.setText(b.getString("stampaj"));
+        dodajDrz.setText(b.getString("drz"));
+        dodajGr.setText(b.getString("gr"));
+    }
+
+    public void engleskiJezik(ActionEvent actionEvent) {
+        l = new Locale("en", "US");
+        b = ResourceBundle.getBundle("Translation", l);
+        View.setText(b.getString("ViewPogled"));
+        Bosanski.setText(b.getString("Bos"));
+        Engleski.setText(b.getString("Eng"));
+        Njemacki.setText(b.getString("Njem"));
+        Francuski.setText(b.getString("Fr"));
+        Language.setText(b.getString("LanugagePrevod"));
+        String s = b.getString("NazivGrada");
+        String s1 = b.getString("brSt");
+        grad.setText(s);
+        brStanovnika.setText(s1);
+        stam.setText(b.getString("stampaj"));
+        dodajDrz.setText(b.getString("drz"));
+        dodajGr.setText(b.getString("gr"));
+    }
+
+    public void njemackiJezik(ActionEvent actionEvent) {
+        l = new Locale("de");
+        b = ResourceBundle.getBundle("Translation", l);
+        View.setText(b.getString("ViewPogled"));
+        Bosanski.setText(b.getString("Bos"));
+        Engleski.setText(b.getString("Eng"));
+        Njemacki.setText(b.getString("Njem"));
+        Francuski.setText(b.getString("Fr"));
+        Language.setText(b.getString("LanugagePrevod"));
+        String s = b.getString("NazivGrada");
+        String s1 = b.getString("brSt");
+        grad.setText(s);
+        brStanovnika.setText(s1);
+        stam.setText(b.getString("stampaj"));
+        dodajDrz.setText(b.getString("drz"));
+        dodajGr.setText(b.getString("gr"));
+    }
+
+    public void francuskiJezik(ActionEvent actionEvent) {
+        l = new Locale("fr");
+        b = ResourceBundle.getBundle("Translation", l);
+        View.setText(b.getString("ViewPogled"));
+        Bosanski.setText(b.getString("Bos"));
+        Engleski.setText(b.getString("Eng"));
+        Njemacki.setText(b.getString("Njem"));
+        Francuski.setText(b.getString("Fr"));
+        Language.setText(b.getString("LanugagePrevod"));
+        String s = b.getString("NazivGrada");
+        String s1 = b.getString("brSt");
+        grad.setText(s);
+        brStanovnika.setText(s1);
+        stam.setText(b.getString("stampaj"));
+        dodajDrz.setText(b.getString("drz"));
+        dodajGr.setText(b.getString("gr"));
+    }
+}
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,51 +1,65 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
-public class Main {
-    static String ispisiGradove() {
-        GeografijaDAO instance = GeografijaDAO.getInstance();
-        final ArrayList<Grad> cities = instance.gradovi();
 
-        StringBuilder result = new StringBuilder();
-        for (Grad city : cities) {
-            result.append(city);
-            result.append("\n");
+public class Main extends Application {
+    static String ispisiGradove(){
+        String s = "";
+        GeografijaDAO gd = new GeografijaDAO();
+        ArrayList<Grad> gradovi = gd.gradovi();
+        ArrayList<Drzava> drzave = gd.drzavaId();
+
+        for(Grad g: gradovi){
+            for(Drzava d: drzave) {
+                if(g.getDrzava().getId() == d.getId())
+                    s += g.getNaziv() + " (" + d.getNaziv() + ") - " + g.getBrojStanovnika() + "\n";
+            }
         }
 
-        return result.toString();
+        return s;
     }
 
-    static void glavniGrad() {
-        GeografijaDAO instance = GeografijaDAO.getInstance();
+    static void glavniGrad(){
+        System.out.println("Unesite naziv drzave: ");
+        String unos;
+        Scanner ulaz = new Scanner(System.in);
+        unos = ulaz.nextLine();
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Unesite ime drzave: ");
-        String country = input.nextLine().trim();
+        GeografijaDAO gd = new GeografijaDAO();
 
-        Grad capital = instance.glavniGrad(country);
-        if (capital != null) {
-            System.out.println(String.format("Glavni grad države %s je %s", country, capital.getNaziv()));
-        } else {
-            System.out.println("Nepostojeća država");
-        }
+        Grad g = gd.glavniGrad(unos);
+        if(g == null) System.out.println("Nepostojeća država");
+        else System.out.println("Glavni grad države " + unos + " je " + g.getNaziv());
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GeografijaDAO model = GeografijaDAO.dajInstancu();
+        Locale.setDefault(new Locale("bs", "BA"));
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("glavni.fxml"), bundle);
+        loader.setController(new GradoviReport(model));
+        Parent root = loader.load();
+
+        /*Parent root = FXMLLoader.load(getClass().getResource("glavni.fxml"));*/
+        primaryStage.setScene(new Scene(root, 339.0, 332.0));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+
+    }
     public static void main(String[] args) {
-        System.out.println(" -- Opcije -----------------");
-        System.out.println("01. Ispiši sve gradove");
-        System.out.println("02. Ispiši glavne gradove");
-        System.out.println();
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Unesite Vaš izbor: ");
-        int option = input.nextInt();
-
-        if (option == 1) {
-            System.out.println(ispisiGradove());
-        } else {
-            glavniGrad();
-        }
+        launch(args);
+        glavniGrad();
+        ispisiGradove();
+        System.out.println(ispisiGradove());
     }
-}
